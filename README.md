b60cae3fb9933aa45f2eb6211f2bf52829d68432
Allen Rohner <arohner@gmail.com>
2015-04-06 16:55:30 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# lein-docker
+
+A Leiningen plugin to perform simple docker tasks. Mainly intended for use in continuous-deployment pipelines.
+
+## Usage
+
+Put `[lein-docker "0.1.0-SNAPSHOT"]` into the `:plugins` vector of your project.clj.
+
+In your project.clj, add a key:
+
+```clojure
+  :docker {:repo "foo/bar"}
+```
+
+`lein docker` supports two commands right now: build and push.
+
+
+### build
+
+    $ lein docker build
+
+Builds a docker image, using the dockerfile at the root of the project
+dir. Uses the repo name from the project.clj, and tags the image with
+the project.clj version.
+
+### push
+
+    $ lein docker push <version>
+
+Performs a simple docker push. Can optionally pass an explicit
+version, or `:latest`, which will push the most recent tag from this
+repo.
+
+## License
+
+Copyright © 2015 Allen Rohner
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/leiningen/docker.clj
@@ -0,0 +1,89 @@
+(ns leiningen.docker
+  (:require [clojure.string :as str]
+            [me.raynes.conch :as sh]
+            [me.raynes.conch.low-level :as shll])
+  (:import java.text.SimpleDateFormat
+           java.util.Date))
+
+(def time-pattern "YYYYMMdd-kkmmss")
+(defn time-string []
+  (->
+   (SimpleDateFormat. time-pattern)
+   (.format (Date.))))
+
+(defn replace-snapshot
+  "Replace a SNAPSHOT in version string with current date-time"
+  [vs]
+  (str/replace vs "SNAPSHOT" (time-string)))
+
+(defn get-version [project]
+  (-> project :version (replace-snapshot)))
+
+(defn sh [& args]
+  (let [p (apply shll/proc args)]
+    (future (shll/stream-to-out p :out))
+    (future (shll/stream-to-out p :err))
+    (shll/exit-code p)))
+
+(defn build* 
+  [repo version]
+  (assert repo "repo is required")
+  (assert version "version is required")
+  (let [tag (str repo ":" version)]
+    (println "building" tag)
+    (sh "docker" "build" "-t" tag ".")))
+
+(defn build
+  ([project args]
+   (let [repo (-> project :docker :repo)
+         version (get-version project)]
+     (build* repo version))))
+
+(defn parse-image-line [line]
+  (-> line
+      (str/split #" ")
+      (->>
+       (filter seq)
+       ((fn [cols]
+          (let [[repo tag image & args] cols]
+            {:repo repo
+             :tag tag
+             :image image}))))))
+
+(defn latest-version [repo]
+  (some-> (sh/execute "docker" "images")
+          (str/split #"\n")
+          (rest)
+          (->>
+           (map parse-image-line)
+           (first)
+           :tag)))
+
+(defn push*
+  [repo version]
+  (assert repo "repo is required")
+  (assert version "version is required")
+  (let [tag (str repo ":" version)]
+    (println "pushing" tag)
+    (sh "docker" "push" tag)))
+
+(defn project-repo [project]
+  (-> project :docker :repo))
+
+(defn push
+  ([project args]
+   (let [repo (-> project :docker :repo)
+         arg-vers (first args)
+         _ (println "arg-vers=" arg-vers)
+         version (cond
+                   (nil? arg-vers) (get-version project)
+                   (= ":latest" arg-vers) (latest-version repo)
+                   :else arg-vers)]
+     (push* repo version))))
+
+(defn docker
+  [project & args]
+  (let [cmd (first args)]
+    (condp = cmd
+      "build" (build project (rest args))
+      "push" (push project (rest args)))))
