b66b481b3b58db03c5556e39f337c85250bff68d
Allen Rohner <arohner@gmail.com>
2015-04-07 16:25:15 -0500

    Add project description, url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject arohner/lein-docker "0.1.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Tools for building and pushing docker containers"
+  :url "https://github.com/arohner/lein-docker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[me.raynes/conch "0.8.0"]]
